75b8cf8611123b7c0552d39575a415cec160843b
Dorien Penebacker <dpenebac@ecegrid-thin1.ecn.purdue.edu>
2021-11-23 15:31:45 -0500

    added writeGrid in text format

--- PA3/pa3.c
+++ PA3/pa3.c
@@ -18,6 +18,20 @@ void printGrid(short r, short c, short *grid)
     printf("\n");  
 }
 
+//write grid in text formatting to file
+void writeGrid(short r, short c, short *grid, FILE* output)
+{
+    int i;
+    for (i = 0; i < r * c; i++)
+    {
+        if ((i + i) % c == 0 && i != 0)
+        {
+            fprintf(output, "\n");
+        }
+        fprintf(output, "%d ", grid[i]);
+    }
+}
+
 int main(int argc, char* argv[])
 {
     FILE* inputGrid = fopen(argv[1], "rb");
@@ -35,24 +49,31 @@ int main(int argc, char* argv[])
     
     short *grid = (short *)malloc((r * c) * sizeof(short));
 
-    short i, temp;
+    int i;
+    short temp;
     for (i = 0; i < r * c; i++)
     {
         fread(&temp, sizeof(temp), 1, inputGrid);
         grid[i] = temp;
     }
 
-    //FILE* inputGridTxt = fopen(argv[2], "w");
+    FILE* inputGridTxt = fopen(argv[2], "w");
     //need to split up into multiple arrays to write line by line???
     printGrid(r, c, grid);
 
+    //write grid in text formatting to file
+    writeGrid(r, c, grid, inputGridTxt);
 
 
 
-    //FILE* inputGridTxt = fopen(argv[2], "w");
+
+
     //FILE* fastestTimes = fopen(argv[1], "wb");
     //FILE* fastestPath = fopen(argv[1], "wb");
 
+
+    fclose(inputGrid);
+    fclose(inputGridTxt);
 
 
     return EXIT_SUCCESS;
